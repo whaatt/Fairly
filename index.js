c4253dde344eb41e06be4e1b8e5bced91ba35f8f
Sanjay Kannan <whaatt@gmail.com>
2014-12-24 16:47:29 -0800

    Add caching to increase speed.

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ var menteeAlias = [
     'duplicate'
 ];
 
+//to serve requests fast
+var dataCacheMentors = [];
+var dataCacheMentees = [];
+var cServed = true;
+
 var mentors = function(req, res) {
     var email = '';
     var code = '';
@@ -105,7 +110,7 @@ var mentors = function(req, res) {
                 delete data[i].address;
                 
                 //no contact info
-                if (auth == false) {
+                if (auth === false) {
                     delete data[i].email;
                     delete data[i].phone;
                 }
@@ -155,6 +160,7 @@ var mentee = function(req, res) {
 };
 
 var getData = function(key, type, call) {
+    cServed = true;
     var options = {
         key : key,
         simpleSheet : true,
@@ -163,29 +169,39 @@ var getData = function(key, type, call) {
             var columns = top.sheets(top.foundSheetNames[0]).column_names;
             
             //change to header aliases
-            if (type == 'mentee') {
+            if (type === 'mentee') {
                 for (var i = 0; i < columns.length; i += 1) {
                     for (var j = 0; j < data.length; j += 1) {
                         data[j][menteeAlias[i]] = data[j][columns[i]];
                         delete data[j][columns[i]];
                     }
                 }
+                
+                //cache mentee data
+                dataCacheMentees = data;
             }
             
             //change to header aliases
-            if (type == 'mentor') {
+            if (type === 'mentor') {
                 for (var i = 0; i < columns.length; i += 1) {
                     for (var j = 0; j < data.length; j += 1) {
                         data[j][mentorAlias[i]] = data[j][columns[i]];
                         delete data[j][columns[i]];
                     }
                 }
+                
+                //cache mentor data
+                dataCacheMentors = data;
             }
             
-            call(data);
+            if (!cServed)
+                call(data);
         }
     }
     
+    if (type === 'mentor' && dataCacheMentors.length > 0) call(dataCacheMentors);
+    else if (type === 'mentee' && dataCacheMentees.length > 0) call(dataCacheMentees);
+    else cServed = false;
     sheets.init(options);
 };
 
